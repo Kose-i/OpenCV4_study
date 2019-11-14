d2fba800167c9070f76c7399e22dd5b8c960d74b
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-11-14 19:17:34 +0900

    Add BasicInfo-GraphicProcessing

--- BasicInformation/src/BasicTransform.cpp
+++ BasicInformation/src/BasicTransform.cpp
@@ -43,8 +43,60 @@ int main(int argc, char** argv) {
   std::cout << "H:" << HSI_H << " S:" << HSI_S << " I:" << HSI_I << '\n';
 
   double XYZ_X, XYZ_Y, XYZ_Z {};
-  XYZ_X = 100*(0.3933*std::pow((RGB_R/255, 2.2)) + 0.3651*std::pow((RGB_G/255, 2.2)) + 0.1903*std::pow(RGB_B/255, 2.2));
-  XYZ_y = 100*(0.2123*std::pow((RGB_R/255, 2.2)) + 0.7010*std::pow((RGB_G/255, 2.2)) + 0.0858*std::pow(RGB_B/255, 2.2));
-  XYZ_Z = 100*(0.0182*std::pow((RGB_R/255, 2.2)) + 0.1117*std::pow((RGB_G/255, 2.2)) + 0.9570*std::pow(RGB_B/255, 2.2));
+  {
+    double tmp_r = std::pow((RGB_R/255.0), 2.2);
+    double tmp_g = std::pow((RGB_G/255.0), 2.2);
+    double tmp_b = std::pow((RGB_B/255.0), 2.2);
+    XYZ_X = 100*(0.3933*tmp_r + 0.3651*tmp_g + 0.1903*tmp_b);
+    XYZ_Y = 100*(0.2123*tmp_r + 0.7010*tmp_g + 0.0858*tmp_b);
+    XYZ_Z = 100*(0.0182*tmp_r + 0.1117*tmp_g + 0.9570*tmp_b);
+  }
   std::cout << "X:" << XYZ_X << " Y:" << XYZ_Y << " Z:" << XYZ_Z << '\n';
+
+  double xyz_x{}, xyz_y{}, xyz_z{};
+  xyz_x = XYZ_X/(XYZ_X+XYZ_Y+XYZ_Z);
+  xyz_y = XYZ_Y/(XYZ_X+XYZ_Y+XYZ_Z);
+  xyz_z = XYZ_Z/(XYZ_X+XYZ_Y+XYZ_Z);
+  std::cout << "x:" << xyz_x << " y:" << xyz_y << " z:" << xyz_z << '\n';
+
+  double CIE_Lab_L{}, CIE_Lab_a{}, CIE_Lab_b{};
+  {
+    double X_0{108.87}, Y_0{100.0}, Z_0{35.59};
+    double tmp_x = std::pow(XYZ_X/X_0, 1/3.0);
+    double tmp_y = std::pow(XYZ_Y/Y_0, 1/3.0);
+    double tmp_z = std::pow(XYZ_Z/Z_0, 1/3.0);
+    CIE_Lab_L = 116*tmp_y - 16;
+    CIE_Lab_a = 500*(tmp_x - tmp_y);
+    CIE_Lab_b = 200*(tmp_y - tmp_z);
+  }
+  std::cout << "L*:" << CIE_Lab_L << " a*:" << CIE_Lab_a << " b*:" << CIE_Lab_b << '\n';
+
+  int CMY_C{}, CMY_M{}, CMY_Y{};
+  CMY_C = 255 - RGB_R;
+  CMY_M = 255 - RGB_G;
+  CMY_Y = 255 - RGB_B;
+  std::cout << "C:" << CMY_C << " M:" << CMY_M << " Y:" << CMY_Y << '\n';
+
+  int CMYK_C{}, CMYK_M{}, CMYK_Y{}, CMYK_K{};
+  {
+    int k = 20;
+    auto ucr = [](int k, const char& color){
+      if(color=='c') return k;
+      if(color=='m') return k;
+      if(color=='y') return k;
+    };
+    auto bg = [](int k) {
+      return k;
+    };
+    int c = 255 - RGB_R;
+    int m = 255 - RGB_G;
+    int y = 255 - RGB_B;
+    CMYK_C = (255 < ((0 > c - ucr(k, 'c'))?0:c-ucr(k, 'c'))?255:((0 > c - ucr(k, 'c'))?0:c-ucr(k, 'c')));
+    CMYK_M = (255 < ((0 > m - ucr(k, 'm'))?0:m-ucr(k, 'm'))?255:((0 > m - ucr(k, 'm'))?0:m-ucr(k, 'm')));
+    CMYK_Y = (255 < ((0 > y - ucr(k, 'y'))?0:y-ucr(k, 'y'))?255:((0 > y - ucr(k, 'y'))?0:y-ucr(k, 'y')));
+    CMYK_K = (255 < ((0 > bg(k))?0:bg(k))?255:((0 > bg(k))?0:bg(k)));
+  }
+  std::cout << "C:" << CMYK_C << " M:" << CMYK_M << " Y:" << CMYK_Y << " K:" << CMYK_K << '\n';
+
+  double YCbCr_Y{}, YCbCr_Cb{}, YCbCr_Cr{};
 }
